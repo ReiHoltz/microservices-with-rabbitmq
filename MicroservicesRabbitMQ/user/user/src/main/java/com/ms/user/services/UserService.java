c6146dd2b25016cdf20693d28f7bca9515acca32
reinaldoholtz <reinaldoholtz@gmail.com>
2025-03-05 16:33:38 +0100

    First commit

---
+++ MicroservicesRabbitMQ/user/user/src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,26 @@
+package com.ms.user.services;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.producer.UserProducer;
+import com.ms.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserProducer userProducer;
+
+    @Transactional
+    public UserModel save(UserModel userModel){
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
+    }
+
+}
